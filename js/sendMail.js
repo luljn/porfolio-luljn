ce7f333c28944e152afebcbaff15a4239710a42a
luljn <mbohlulajonathan4@gmail.com>
2024-07-08 02:01:44 +0200

    Send mail feature refactored

---
+++ js/sendMail.js
@@ -0,0 +1,6 @@
+document.getElementById("sendEmailButton").addEventListener("click", function() {
+
+    var email = "mbohlulajonathan4@gmail.com";
+    window.location.href = `mailto:${email}`;
+    }
+)
--- js/tooltips.js
+++ js/tooltips.js
@@ -2,8 +2,3 @@ var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggl
 var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
     return new bootstrap.Tooltip(tooltipTriggerEl)
 })
-
-function openMailClient(){
-
-    document.getElementById('mailForm').submit();
-}
