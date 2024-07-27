c9dbd162207d33111b18fa18b23a350a75f97336
luljn <mbohlulajonathan4@gmail.com>
2024-07-27 18:43:06 +0200

    Download CV feature added

---
+++ js/downloadCV.js
@@ -0,0 +1,18 @@
+document.getElementById('downloadButton').addEventListener('click', function() {
+    // URL of the PDF file
+    var pdfUrl = '../files/CV-Lula_Jonathan_MBECK_MBOH.pdf';
+
+    // Create a new anchor element
+    var a = document.createElement('a');
+    a.href = pdfUrl;
+    a.download = 'CV-Lula_Jonathan_MBECK_MBOH.pdf'; // Name of the file to be downloaded
+
+    // Append the anchor to the body (necessary for Firefox)
+    document.body.appendChild(a);
+
+    // Trigger the download by simulating a click
+    a.click();
+
+    // Remove the anchor from the body
+    document.body.removeChild(a);
+});
